37e437e52af0d25893370b77c50d5902360b1ae5
Samagra Gupta <gsamagra93@gmail.com>
2026-07-08 14:38:50 +0530

    INPUTOUTPUT

---
+++ InputOutput.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "Hey Striver!" << endl << "SAMAGRA" << endl;
+    cout << "Hey Striver!" << endl ;
+    int x , y;
+    cin >> x >> y; 
+
+    cout << "Value of X is: " << x << endl;
+    cout << "Value of Y is: " << y << endl;
+
+    return 0;
+}
